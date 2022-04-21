4c786413e79272c0715d6fe845b3bd247d8d9101
hind <hindsamiri2@gmail.com>
2022-04-21 19:37:57 +0200

    tarea7A

---
+++ src/main/java/tarea7AHind/Empleado.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea7AHind;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ *
+ * @author hinda
+ */
+public class Empleado {
+    //Atributos
+   private String nombre;
+    private String dni;
+    private String puesto;
+    private LocalDate fechaTomaPos;
+    private LocalDate fechaCese;
+    private String telfono;
+    private boolean evaluador;
+    private boolean coordinador;
+    
+    //constrector
+    public Empleado() {
+    }
+    
+    //getters y setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public LocalDate getFechaTomaPos() {
+        return fechaTomaPos;
+    }
+
+    public void setFechaTomaPos(LocalDate fechaTomaPos) {
+        this.fechaTomaPos = fechaTomaPos;
+    }
+
+    public LocalDate getFechaCese() {
+        return fechaCese;
+    }
+
+    public void setFechaCese(LocalDate fechaCese) {
+        this.fechaCese = fechaCese;
+    }
+
+    public String getTelfono() {
+        return telfono;
+    }
+
+    public void setTelfono(String telfono) {
+        this.telfono = telfono;
+    }
+
+    public boolean isEvaluador() {
+        return evaluador;
+    }
+
+    public void setEvaluador(boolean evaluador) {
+        this.evaluador = evaluador;
+    }
+
+    public boolean isCoordinador() {
+        return coordinador;
+    }
+
+    public void setCoordinador(boolean coordinador) {
+        this.coordinador = coordinador;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + Objects.hashCode(this.nombre);
+        hash = 31 * hash + Objects.hashCode(this.dni);
+        hash = 31 * hash + Objects.hashCode(this.puesto);
+        hash = 31 * hash + Objects.hashCode(this.fechaTomaPos);
+        hash = 31 * hash + Objects.hashCode(this.fechaCese);
+        hash = 31 * hash + Objects.hashCode(this.telfono);
+        hash = 31 * hash + (this.evaluador ? 1 : 0);
+        hash = 31 * hash + (this.coordinador ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Empleado other = (Empleado) obj;
+        if (this.evaluador != other.evaluador) {
+            return false;
+        }
+        if (this.coordinador != other.coordinador) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.dni, other.dni)) {
+            return false;
+        }
+        if (!Objects.equals(this.puesto, other.puesto)) {
+            return false;
+        }
+        if (!Objects.equals(this.telfono, other.telfono)) {
+            return false;
+        }
+        if (!Objects.equals(this.fechaTomaPos, other.fechaTomaPos)) {
+            return false;
+        }
+        if (!Objects.equals(this.fechaCese, other.fechaCese)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return  nombre + ";" + 
+                dni + ";" + puesto + ";" + 
+                fechaTomaPos + ";" + fechaCese + ";" + 
+                telfono + ";" + evaluador + ";" + coordinador;
+    }
+
+   
+    
+    
+    
+    
+}
---
+++ src/main/java/tarea7AHind/LecturaFichero.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea7AHind;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author hinda
+ */
+public class LecturaFichero {
+
+    public static ArrayList<Empleado> lecturaFichero() {
+        // Fichero a leer
+        String idFichero = "RelPerCen.csv";
+
+        // Variables para guardar los datos que se van leyendo
+        //lista
+        String[] tokens;
+        String linea;
+        ArrayList<Empleado> empleados = new ArrayList<>();
+
+        System.out.println("Leyendo el fichero: " + idFichero);
+        try (Scanner datosFichero = new Scanner(new File(idFichero), "ISO_8859_1")) {
+            datosFichero.nextLine();
+            // hasNextLine devuelve true mientras haya líneas por leer
+            while (datosFichero.hasNextLine()) {
+                // Guarda la línea completa en un String
+                linea = datosFichero.nextLine();
+                linea = linea.replaceAll("\"", "");//para quitar las comillas dobles
+                // Se guarda en el array de String cada elemento de la linea 
+                //separando con coma
+                tokens = linea.split(",");
+
+                //crear objeto empleado
+                Empleado emp = new Empleado();
+
+                emp.setNombre(tokens[0].concat(tokens[1]));
+                emp.setDni(tokens[2]);
+                emp.setPuesto(tokens[3]);
+
+                emp.setFechaTomaPos(LocalDate.parse(tokens[4], DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+                emp.setFechaCese(tokens[5].isEmpty() ? LocalDate.parse(LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")), DateTimeFormatter.ofPattern("dd/MM/yyyy")) : LocalDate.parse(tokens[5], DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+                emp.setTelfono(tokens[6]);
+                emp.setEvaluador(Boolean.parseBoolean(tokens[7]));
+                emp.setCoordinador(Boolean.parseBoolean(tokens[8]));
+                empleados.add(emp);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return empleados;
+    }
+
+    public static Map<String, Integer> GenerarMapaEmpleados(ArrayList<Empleado> lista) {
+        Map<String, Integer> mapa = new HashMap<>();
+        int cnt = 1;
+        for (Empleado obj : lista) {
+            if (mapa.containsKey(obj.getPuesto())) {
+                cnt++;
+                mapa.put(obj.getPuesto(), cnt);
+            } else {
+                mapa.put(obj.getPuesto(), 1);
+            }
+        }
+        return mapa;
+    }
+
+    /*Guarda en otro fichero CSV los registros de aquellos empleados que hayan trabajado más de 100 días en el curso 20/21*/
+    public static ArrayList<Empleado> generarTrabajadorplus100(ArrayList<Empleado> lista) {
+        ArrayList<Empleado> listaAux = new ArrayList<>();
+        for (Empleado obj : lista) {
+            //si los dias son mas que 100 añadimos este trabajador a la lista
+            if (daysBetween(obj.getFechaTomaPos(), obj.getFechaCese()) > 100) {
+                listaAux.add(obj);
+            }
+        }
+        return listaAux;
+    }
+
+    //para calcular los dias de trabajo entre la fecha ini y fin 
+    public static int daysBetween(LocalDate dateIni, LocalDate dateFin) {
+        int numberDays;
+        LocalDate fechaIni=LocalDate.of(2020,1,1);
+        LocalDate fechaFin=LocalDate.of(2021, 12, 31);
+        //si la fecha de inicio que pasemos como parametro is after 2020 y la fecha de cese is befor 2021
+        //entonces esta dentro del rango que queremos
+        if (dateIni.isAfter(fechaIni) && dateFin.isBefore(fechaFin)) {
+            //calcular numero de dias hay 
+            numberDays = (int) ChronoUnit.DAYS.between(dateIni, dateFin);
+        } else {
+            numberDays = 0;
+        }
+        return numberDays;
+        
+    }
+
+    public static void escrituraEnFicheroTrabajadoresMas100(ArrayList<Empleado> lista) {
+        // Fichero a crear. Ruta relativa a la carpeta raíz del proyecto
+        String idFichero = "registrosTrabajores100Dias.csv";
+        ArrayList<Empleado> listaAux = generarTrabajadorplus100(lista);
+
+        try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero))) {
+            //recorrer la lista de empleados generada y escribir en el fichero
+            for (Empleado obj : listaAux) {
+                //añadir el numero de dias trabajan
+                flujo.write(obj + "\t"+daysBetween(obj.getFechaTomaPos(),obj.getFechaCese()));
+                flujo.newLine();
+            }
+            // Metodo fluh() guarda cambios en disco 
+            flujo.flush();
+            System.out.println("Fichero " + idFichero + " creado correctamente.");
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void escrituraFichero() {
+        // Fichero a crear. Ruta relativa a la carpeta raíz del proyecto
+        String idFichero = "profesoresPorDepartamento.csv";
+        Map<String, Integer> mapa = GenerarMapaEmpleados(lecturaFichero());
+
+        try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero))) {
+            for (Map.Entry<String, Integer> entry : mapa.entrySet()) {
+                flujo.write(entry.getKey() + "\t" + entry.getValue());
+                flujo.newLine();
+            }
+
+            // Metodo fluh() guarda cambios en disco 
+            flujo.flush();
+            System.out.println("Fichero " + idFichero + " creado correctamente.");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        //lesta empleados a partir del fichero
+        ArrayList<Empleado> empleados = lecturaFichero();
+
+        //total profesores existen en el fichero
+        System.out.println("el total de profesores existen es: " + empleados.size());
+        //muestra la lista de empleados
+        empleados.forEach(System.out::println);
+
+        /*Genera un map para almacenar cuantos profesores hay por cada departamento diferente. 
+        Vuelca esta información en otro fichero CSV, llamado "profesoresPorDepartamento.csv", separando los campos con un tabulador.*/
+        escrituraFichero();
+        
+        /*Guarda en otro fichero CSV los registros de aquellos empleados que hayan trabajado más de 100 días en el curso 20/21. */
+        
+        escrituraEnFicheroTrabajadoresMas100(empleados);
+
+    }
+}
